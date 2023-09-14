e5577afc42c0fde728e4ccb7dd0b080a084f7317
BlueBlack22 <115193822+BlueBlack22@users.noreply.github.com>
2023-09-14 19:34:32 +0200

    Added eventListeners to nav tabs.

--- src/index.js
+++ src/index.js
@@ -10,4 +10,32 @@ const container = document.createElement('div');
 container.setAttribute('id', 'container');
 content.appendChild(container);
 
-container.appendChild(createHome());
+function createPage(tab) {
+    container.innerText = '';
+
+    switch (tab) {
+        case 'home': {
+            container.appendChild(createHome());
+            break;
+        }
+        case 'menu': {
+            container.appendChild(createMenu());
+            break;
+        }
+        case 'contact': {
+            container.appendChild(createContact());
+            break;
+        }
+    }
+};
+
+const menuTab = document.getElementById('menu-tab');
+menuTab.addEventListener('click', (e) => createPage('menu'));
+
+const homeTab = document.getElementById('home-tab');
+homeTab.addEventListener('click', (e) => createPage('home'));
+
+const contactTab = document.getElementById('contact-tab');
+contactTab.addEventListener('click', (e) => createPage('contact'));
+
+createPage('home');
