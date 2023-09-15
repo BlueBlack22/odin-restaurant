2d64bd0d044bff5958881fdd507b585dca14e2e8
BlueBlack22 <115193822+BlueBlack22@users.noreply.github.com>
2023-09-15 18:51:26 +0200

    Added contact page text.

--- src/contact.js
+++ src/contact.js
@@ -1,8 +1,26 @@
 function createContact() {
-    const contact = createDiv(contact);
+    const contact = createDiv('contact');
 
-    contact.appendChild(createParagraph('Opening hours', 'contact-header'));
+    contact.appendChild(createParagraph('Contact', 'contact-header'));
     
+    const hours = createDiv(undefined, 'contact-hours');
+    hours.appendChild(createParagraph('Opening hours', 'contact-section-header'));
+    
+    const hoursList = document.createElement('ul');
+    hoursList.appendChild(createLi('Monday: closed'));
+    hoursList.appendChild(createLi('Monday - Friday: 1pm - 9pm'));
+    hoursList.appendChild(createLi('Saturday - Sunday: 2pm - 10pm'));
+    hours.appendChild(hoursList);
+    contact.appendChild(hours);
+
+    const location = createDiv(undefined, 'contact-location');
+    location.appendChild(createParagraph('Location', 'contact-section-header'));
+    
+    const map = document.createElement('img');
+    map.src = '';
+    map.alt = 'map picture';
+    location.appendChild(map);
+    contact.appendChild(location);
 
     return contact;
 };
@@ -19,6 +37,13 @@ function createParagraph(text, className) {
 
     return para;
 };
+
+function createLi(text) {
+    const li = document.createElement('li');
+    li.textContent = text;
+
+    return li;
+}
 
 function createDiv(id, className) {
     const div = document.createElement('div');
